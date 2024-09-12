e53e26d9eb1436d8b29e650de8c258c4c2813904
Sumbal Naz <Codeq209@gmail.com>
2024-09-12 16:34:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+console.log("\n\tWELCOME TO\'CODE WITH SNAZ'\-CLI PROJECT(CALCULATOR)\n")
+
+const answer = await inquirer.prompt([
+    {message: "enter first number", type: "number", name: "firstnumber"},
+    {message: "enter second number", type: "number", name: "secondnumber"},
+    {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "substraction", "multiplication", "Division"],
+    },
+]);
+console.log(answer);
+
+
+if(answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+} else if(answer.operator === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+} else if(answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+} else if(answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+} else {
+    console.log("please select valid operator");
+}
